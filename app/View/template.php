82fbfde5304d815367ab6c4ee16e2645faf3515f
Youness Id bakkasse <std.youness@gmail.com>
2022-01-07 19:35:10 +0100

    Added a responsive header.

--- app/View/template.php
+++ app/View/template.php
@@ -11,19 +11,102 @@
 </head>
 <body>
     <header>
-        <div>
-            <img src='' alt='logo'>
+        <div class="relative bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div class="flex justify-start lg:w-0 lg:flex-1">
+                <a href="#" class="text-2xl font-medium text-gray-600">
+                    Polonium
+                </a>
+            </div>
+            
+            <div class="-mr-2 -my-2 md:hidden">
+                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-amber-500" aria-expanded="false">
+                <span class="sr-only">Open menu</span>
+                
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                </button>
+            </div>
+            
+            <nav class="hidden md:flex space-x-10">
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Homepage
+                </a>
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Featured articles
+                </a>
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+                    All articles
+                </a>
+            </nav>
+            <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                    Sign in
+                </a>
+                <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-amber-600 hover:bg-amber-700">
+                    Sign up
+                </a>
+            </div>
+            </div>
         </div>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/write-article">Wrtie article</a></li>
-                <li><a href="/user/list">User list</a></li>
-            </ul>
-        </nav>
-        <div class="sign-cta">
-            <a class="text-3xl font-bold underline" href="/user/signin">Sign In</a>
-            <a href="/user/signup">Sign Up</a>
+
+        <!--
+            Mobile menu, show/hide based on mobile menu state.
+        -->
+        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+            <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div class="pt-5 pb-6 px-5">
+                <div class="flex items-center justify-between">
+                <div>
+                    <a href="#" class="text-2xl font-medium text-gray-600">Polonium</a>
+                </div>
+                <div class="-mr-2">
+                    <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-amber-500">
+                    <span class="sr-only">Close menu</span>
+                    <!-- Heroicon name: outline/x -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                </div>
+                </div>
+                <div class="mt-6">
+                    <nav class="grid gap-y-8">
+                        <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                            <span class="ml-3 text-base font-medium text-gray-900">
+                                Homepage
+                            </span>
+                        </a>
+                        <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                            <span class="ml-3 text-base font-medium text-gray-900">
+                                Featured articles
+                            </span>
+                        </a>
+                        <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                            <span class="ml-3 text-base font-medium text-gray-900">
+                                All articles
+                            </span>
+                        </a>
+                    </nav>
+                </div>
+            </div>
+            <div class="py-6 px-5 space-y-6">
+                <div>
+                    <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-amber-600 hover:bg-amber-700">
+                        Sign up
+                    </a>
+                    <p class="mt-6 text-center text-base font-medium text-gray-500">
+                        Existing customer?
+                        <a href="#" class="text-amber-600 hover:text-amber-500">
+                        Sign in
+                        </a>
+                    </p>
+                </div>
+            </div>
+            </div>
+        </div>
         </div>
     </header>
 
