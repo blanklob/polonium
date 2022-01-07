b543cec91d9c7dc6c6f8245ee896cd4869ec1913
Sooseki <bonnetmanon3006@gmail.com>
2022-01-07 15:16:40 +0100

    [html/css] header base

--- app/View/template.php
+++ app/View/template.php
@@ -5,11 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="src/main.css">
     <title><?= $title; ?></title>
 </head>
 <body>
+    <header>
+        <div>
+            <img src='' alt='logo'>
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Wrtie article</li>
+                <li>User list</li>
+            </ul>
+        </nav>
+        <div class="sign-cta">
+            <a href="#">Sign In</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </header>
 
-<?= $content; ?>
+    <main>
+        <?= $content; ?>
+    </main>
 
 </body>
 </html>
